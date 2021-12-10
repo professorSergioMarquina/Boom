3885a4a182ea727a5cff5fda2cfeda7df47ebf98
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2021-12-10 23:20:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -150,7 +150,7 @@ async def channel_info(bot, message):
     else:
         raise ValueError("Unexpected type of CHANNELS")
 
-    text = '📑 **Indexed channels/groups**\n'
+    text = '🔰 **Indexed channels/groups**\n'
     for channel in channels:
         chat = await bot.get_chat(channel)
         if chat.username:
