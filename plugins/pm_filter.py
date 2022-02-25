8185ad8370dc1d7c427ab38a0f42273b3a2d32de
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-02-25 23:32:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -146,7 +146,7 @@ async def next_page(bot, query):
         btn.append(
             [
                 InlineKeyboardButton("⏪ 𝐁𝐚𝐜𝐤", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"🔰{round(int(offset)/10)+1}𝐭𝐡 𝐏𝐚𝐠𝐞🔰", callback_data="pages"),
+                InlineKeyboardButton(f"{round(int(offset)/10)+1}𝐭𝐡 𝐏𝐚𝐠𝐞", callback_data="pages"),
                 InlineKeyboardButton(" 𝐍𝐞𝐱𝐭 ⏩", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
