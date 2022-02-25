26f5ad010597a3cbd1bd21cac37916e913a16686
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-02-25 23:04:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -100,8 +100,8 @@ async def get_poster(query, bulk=False, id=False):
             plot = plot[0]
     else:
         plot = movie.get('plot outline')
-    if plot and len(plot) > 50:
-        plot = plot[0:50] + "..."
+    if plot and len(plot) > 500:
+        plot = plot[0:500] + "..."
 
     return {
         'title': movie.get('title'),
